3902191eb48feb1c5a73cda5bde96f62cfdbed44
Bruno Carvalho <brunopessoal888@gmail.com>
2025-03-17 07:53:45 -0300

    subindo configuração do script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 ﻿fetch('/api/sunburst')
   .then(response => response.json())
   .then(data => {
-    // Criação do gráfico Plotly
     const trace = {
       type: 'sunburst',
       labels: data.labels,
       parents: data.parents,
       values: data.values,
       customdata: data.customdata,
-      hovertemplate: '<b>%{label}</b><br>%{customdata}<extra></extra>',
-      branchvalues: 'total'
+      hovertemplate: '<b>%{label}</b><br>%{customdata}<br>Deputados: %{value}<extra></extra>',
+      branchvalues: 'total',
+      
+      // Aqui você controla o que exibe nos rótulos do gráfico
+      textinfo: 'label+value', // Mostra o nome e o número de deputados
+      insidetextorientation: 'radial'
     };
 
     const layout = {
       title: 'Composição da Bancada Evangélica da ALEPE',
-      margin: { l: 0, r: 0, b: 0, t: 30 },
-      height: 300
+      margin: { l: 0, r: 0, b: 0, t: 50 },
+      height: 400
     };
 
     Plotly.newPlot('sunburst-graph', [trace], layout);
