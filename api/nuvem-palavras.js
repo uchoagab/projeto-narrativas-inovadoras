63f0f6027c2bbe9b3e03b4e51d52cac272918b61
CharlysFarias21 <charlysaugusto@prottus.com.br>
2025-03-28 12:15:04 -0300

    Fix bugs

--- api/nuvem-palavras.js
+++ api/nuvem-palavras.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
         { palavra: <a href="https://www.alepe.pe.gov.br/proposicao-texto-completo/?docid=8330&tipoprop=p">Cristofobia</a>, tamanho: 100 },
         { palavra: <a href="https://www.alepe.pe.gov.br/proposicao-texto-completo/?docid=13095&tipoprop=p">O que é mulher?</a>, tamanho: 340 },
         { palavra: <a href="https://www.alepe.pe.gov.br/proposicao-texto-completo/?docid=10045&tipoprop=p">Proibição de transição de gênero em menores de 18 mesmo com consentimento dos pai</a>, tamanho: 108 },
-
+    ]
     // Responde com os dados JSON
     res.status(200).json(data);
 }
