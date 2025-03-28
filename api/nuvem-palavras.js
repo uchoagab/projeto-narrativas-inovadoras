4799b95fa605faabbbd2208fcb2b370481ea90cf
CharlysFarias21 <charlysaugusto@prottus.com.br>
2025-03-28 12:12:55 -0300

    Update nas nuvem de palavras

--- api/nuvem-palavras.js
+++ api/nuvem-palavras.js
@@ -1,14 +1,10 @@
 ﻿// Função serverless para servir os dados da nuvem de palavras
 export default async function handler(req, res) {
     const data = [
-        { palavra: "Saúde", tamanho: 100 },
-        { palavra: "Educação", tamanho: 100 },
-        { palavra: "Infraestrutura", tamanho: 340 },
-        { palavra: "Meio Ambiente", tamanho: 108 },
-        { palavra: "Tecnologia", tamanho: 209 },
-        { palavra: "Segurança", tamanho: 502 },
-        { palavra: "Cultura", tamanho: 100 }
-    ];
+        { palavra: <a href="https://www.alepe.pe.gov.br/proposicao-texto-completo/?docid=8516&tipoprop=p">Preconceito contra Antivax</a>, tamanho: 100 },
+        { palavra: <a href="https://www.alepe.pe.gov.br/proposicao-texto-completo/?docid=8330&tipoprop=p">Cristofobia</a>, tamanho: 100 },
+        { palavra: <a href="https://www.alepe.pe.gov.br/proposicao-texto-completo/?docid=13095&tipoprop=p">O que é mulher?</a>, tamanho: 340 },
+        { palavra: <a href="https://www.alepe.pe.gov.br/proposicao-texto-completo/?docid=10045&tipoprop=p">Proibição de transição de gênero em menores de 18 mesmo com consentimento dos pai</a>, tamanho: 108 },
 
     // Responde com os dados JSON
     res.status(200).json(data);
