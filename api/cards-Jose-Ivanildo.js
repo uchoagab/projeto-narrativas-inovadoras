7683aab4da7936048d2dfa17a87649448e6a4a71
Bruno Carvalho <brunopessoal888@gmail.com>
2025-04-08 21:02:31 -0300

    Subindo alterações

--- api/cards-Jose-Ivanildo.js
+++ api/cards-Jose-Ivanildo.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
       nome: "José Ivanildo de Moura Júnior",
       partido: "PP",
       descricao:
-        "José Ivanildo de Moura Júnior (Pastor Júnior Tércio) é um pastor e radialista. Deputado mais votado em 2022 em PE, na ALEPE tem como participações ser Vice-presidente da Comissão de Cidadania, integrou a comissão de Educação, Cultura, Esporte e Lazer",
+        "José Ivanildo de Moura Júnior (Pastor Júnior Tércio) é um pastor e radialista. Deputado mais votado em 2022 em PE, na ALEPE tem como participações ser Vice-presidente da Comissão de Cidadania, integrou  a comissão de Educação, Cultura, Esporte e Lazer",
       foto: "/images/jose_ivanildo.jpg",
       votos: 183.735,
       candidatura: "2023-Hoje",
