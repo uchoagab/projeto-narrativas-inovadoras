0e7294d4c9740b19545576459295531a0e7ccf59
Bruno Carvalho <brunopessoal888@gmail.com>
2025-04-11 07:21:25 -0300

    Subindo novos dados em votos totais

--- api/votos.js
+++ api/votos.js
@@ -1,5 +1,4 @@
-﻿
-export default function handler(req, res) {
+﻿export default function handler(req, res) {
   const dados = {
     anos: ["2014", "2018", "2022"],
     partidos: [
@@ -9,23 +8,23 @@ export default function handler(req, res) {
         percentual: [8.06, 17.57, 14.36],
       },
       {
-        nome: "União (DEM/PSL)",
-        votos: [69199, 178253 + 89682, 460153],
-        percentual: [1.50, 3.95 + 1.99, 9.18],
+        nome: "União (fusão DEM/PSL)",
+        votos: [69199 + 88315, 178253 + 89682, 460153],
+        percentual: [1.50 + 1.92, 3.95 + 1.99, 9.18],
       },
       {
-        nome: "PL",
-        votos: [0, 0, 422456],
-        percentual: [0, 0, 8.43],
+        nome: "PL (ex-PR)",
+        votos: [158914, 116524, 422456],
+        percentual: [3.75, 2.58, 8.43],
       },
       {
-        nome: "Republicanos",
-        votos: [0, 0, 235974],
-        percentual: [0, 0, 4.71],
+        nome: "Republicanos (ex-PRB)",
+        votos: [50572, 59429, 235974],
+        percentual: [1.1, 1.32, 4.71],
       },
     ],
-    total: [440259, 1060452, 1838688],
-    percentualTotal: [9.59, 23.51, 36.68],
+    total: [738060, 1236405, 1838688],
+    percentualTotal: [16.33, 27.41, 36.68]
   };
 
   res.status(200).json(dados);
